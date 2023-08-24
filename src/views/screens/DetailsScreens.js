c29d1278dd564a85c442f8caa5abfe4e927055f9
Burak BAKAN <bbakan323@gmail.com>
2023-08-24 17:24:54 +0300

    push

---
+++ src/views/screens/DetailsScreens.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import { ImageBackground, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import COLORS from '../../consts/colors';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const DetailsScreen = ({ navigation, route }) => {
+  const item = route.params;
+  console.log(item);
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ backgroundColor: COLORS.white, paddingBottom: 20, }}>
+      <StatusBar barStyle="light-content" transculent backgroundColor="rgba(0,0,0,0)" />
+      <ImageBackground style={styles.headerImage} source={item.image}>
+        <View style={styles.header}>
+          <Icon name="bookmark-border" size={28} color={COLORS.white} />
+        </View>
+      </ImageBackground>
+      <View>
+        <View style={styles.iconContainer}>
+          <Icon name="place" color={COLORS.white} size={28} />
+        </View>
+        <View style={{ marginTop: 20, paddingHorizontal: 20 }}>
+          <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{item.name}</Text>
+          <Text style={{ fontSize: 12, fontWeight: '400', color: COLORS.grey, marginTop: 5 }}>{item.location}</Text>
+          <View style={{ marginTop: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
+            <View style={{ flexDirection: 'row' }}>
+              <View style={{ flexDirection: 'row' }}>
+                <Icon name="star" size={20} color={COLORS.orange} />
+                <Icon name="star" size={20} color={COLORS.orange} />
+                <Icon name="star" size={20} color={COLORS.orange} />
+                <Icon name="star" size={20} color={COLORS.orange} />
+                <Icon name="star" size={20} color={COLORS.grey} />
+              </View>
+              <Text style={{ fontWeight: "bold", fontSize: 18, marginLeft: 5 }}>4.0
+              </Text>
+            </View>
+            <Text style={{ fontSize: 13, color: COLORS.grey }}>365 Views</Text>
+          </View>
+          <View style={{ marginTop: 20 }}>
+            <Text style={{ lineHeight: 20, color: COLORS.grey }}>{item.details}</Text>
+          </View>
+        </View>
+      </View>
+      <View style={{ marginTop: 20, flexDirection: 'row', justifyContent: 'space-between', paddingLeft: 20, alignItems: 'center' }}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Price per night</Text>
+        <View style={styles.priceTag}>
+          <Text style={{ fontSize: 16, fontWeight: 'bold', color: COLORS.grey }}>${item.price}</Text>
+          <Text style={{ fontSize: 12, fontWeight: 'bold', color: COLORS.grey }}>+breakfast</Text>
+        </View>
+        <View style={styles.btn}></View>
+        <Text style={{color:COLORS.white, fontSize:18, fontWeight:'bold'}}>Book Now</Text>
+      </View>
+    </ScrollView >
+  );
+};
+
+const styles = StyleSheet.create({
+  btn: {
+    height:55,
+    justifyContent:'center',
+    alignItems:'center',
+    marginTop:40,
+    backgroundColor: COLORS.primary,
+    marginHorizontal:20,
+    borderRadius:10,
+  },
+  priceTag: {
+    height: 40,
+    alignItems: 'center',
+    marginLeft: 40,
+    paddingLeft: 20, backgroundColor: COLORS.secondary,
+    borderTopLeftRadius: 20,
+    borderBottomLeftRadius: 20,
+    flexDirection: 'row',
+
+  },
+  headerImage: {
+    height: 400,
+    borderBottomRightRadius: 40,
+    borderBottomLeftRadius: 40,
+    overflow: 'hidden',
+  },
+  header: {
+    marginTop: 60,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    justifyContent: "space-between"
+  },
+  iconContainer: {
+    position: 'absolute',
+    height: 60,
+    width: 60,
+    backgroundColor: COLORS.primary,
+    top: -30,
+    right: 20,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+export default DetailsScreen;
